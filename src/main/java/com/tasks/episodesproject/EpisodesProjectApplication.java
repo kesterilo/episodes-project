fe56924abda01625d6c306fe10beaae71b4807a5
Kester <ilokester@gmail.com>
2023-10-07 17:59:28 +0100

    First commit - initialized project and create docker files

---
+++ src/main/java/com/tasks/episodesproject/EpisodesProjectApplication.java
@@ -0,0 +1,13 @@
+package com.tasks.episodesproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpisodesProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpisodesProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tasks/episodesproject/EpisodesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tasks.episodesproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpisodesProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
